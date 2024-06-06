cf8ac9b9430e6232a723d65bbf5358f362a5e250
Ieva Šinke <ieva.sinke@gmail.com>
2024-06-06 12:46:25 +0300

    search results table

--- index.php
+++ index.php
@@ -1,2 +1,42 @@
 <?php
 
+require_once 'vendor/autoload.php';
+
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Helper\TableCell;
+use Symfony\Component\Console\Helper\TableCellStyle;
+use Symfony\Component\Console\Output\ConsoleOutput;
+
+
+$input = (string)readline("Enter company name: ");
+
+$searchQuery = urlencode($input);
+$resource_id = "25e80bf3-f107-4ab4-89ef-251b5b9374e9";
+$url = "https://data.gov.lv/dati/lv/api/3/action/datastore_search?q=$searchQuery&resource_id=$resource_id";
+
+$companies = json_decode(file_get_contents($url));
+
+if (empty($companies)) {
+    exit("Failed to fetch data. Please try again later.\n");
+}
+
+$records = $companies->result->records;
+
+$outputTasks = new ConsoleOutput();
+$table = new Table($outputTasks);
+
+$table->setHeaderTitle('Companies');
+$table->render();
+$table
+    ->setHeaders(['Registration number', 'Name', 'Type', 'Registered at', 'Address'])
+    ->setRows(array_map(function (stdClass $record): array {
+        return [
+            $record->sepa,
+            new TableCell($record->name, ['style' => new TableCellStyle(['align' => 'center',])]),
+            $record->type,
+            $record->registered,
+            $record->address,
+        ];
+    }, $records));
+$table->setStyle('box-double');
+$table->render();
